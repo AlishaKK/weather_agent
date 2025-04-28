ebc598d60d0c21f997f12e78e6ec2f1552efbfa9
AlishaKK <nidakayani043@gmail.com>
2025-04-29 04:41:41 +0500

    commit

--- src/myapp/main.py
+++ src/myapp/main.py
@@ -5,6 +5,7 @@ from dotenv import get_key,find_dotenv
 import requests
 
 gemini_api_key=get_key(find_dotenv(),"GEMINI_API_KEY")
+weather_api_key=get_key(find_dotenv(),"WEATHER_API_KEY")
 
 
 client=AsyncOpenAI(
@@ -18,7 +19,7 @@ def get_weather(city:str)->str:
     """
     Get the current weather for a given city.
     """
-    result=requests.get(f"http://api.weatherapi.com/v1/current.json?key=8e3aca2b91dc4342a1162608252604&q={city}")
+    result=requests.get(f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q={city}")
     data=result.json()
     return f"The current weather in {city} is {data['current']['temp_c']}°C with {data['current']['condition']['text']}."
 
